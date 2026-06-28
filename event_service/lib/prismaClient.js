c7b8f6c649eefb20a9e90fcf3cd86e921e8868a5
chamodpw2000 <mailtochamodw@gmail.com>
2026-06-28 17:32:44 +0530

    db issue

--- event_service/lib/prismaClient.js
+++ event_service/lib/prismaClient.js
@@ -1,13 +1,28 @@
+// import prismaPkg from "@prisma/client";
+// import "dotenv/config";
+// import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+// const { PrismaClient } = prismaPkg;
+// const adapter = new PrismaMariaDb(process.env.DATABASE_URL);
+
+// export const prisma = new PrismaClient({
+//   adapter,
+// });
+
+// export const connectPrisma = async () => {
+//   await prisma.$connect();
+// };
+
+// export const disconnectPrisma = async () => {
+//   await prisma.$disconnect();
+// };
 import prismaPkg from "@prisma/client";
 import "dotenv/config";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
 const { PrismaClient } = prismaPkg;
-const adapter = new PrismaMariaDb(process.env.DATABASE_URL);
 
-export const prisma = new PrismaClient({
-  adapter,
-});
+// Prisma natively supports your mysql:// connection string out of the box!
+export const prisma = new PrismaClient();
 
 export const connectPrisma = async () => {
   await prisma.$connect();
@@ -16,3 +31,4 @@ export const connectPrisma = async () => {
 export const disconnectPrisma = async () => {
   await prisma.$disconnect();
 };
+
