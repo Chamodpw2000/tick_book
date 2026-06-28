76784b14c0ae3932488e6addcf33d70553df5eb4
chamodpw2000 <mailtochamodw@gmail.com>
2026-06-28 18:21:26 +0530

    db issue 5

--- event_service/lib/prismaClient.js
+++ event_service/lib/prismaClient.js
@@ -1,36 +1,9 @@
-// import prismaPkg from "@prisma/client";
-// import "dotenv/config";
-// import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-
-// const { PrismaClient } = prismaPkg;
-// const adapter = new PrismaMariaDb(process.env.DATABASE_URL);
-
-// export const prisma = new PrismaClient({
-//   adapter,
-// });
-
-// export const connectPrisma = async () => {
-//   await prisma.$connect();
-// };
-
-// export const disconnectPrisma = async () => {
-//   await prisma.$disconnect();
-// };
-import { PrismaClient } from "@prisma/client";
+import prismaPkg from "@prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import "dotenv/config";
 
-// Parse your string safely
-const dbUrl = new URL(process.env.DATABASE_URL);
-
-// Initialize with a clean configuration object
-const adapter = new PrismaMariaDb({
-  host: dbUrl.hostname,
-  port: parseInt(dbUrl.port || "3306"),
-  user: dbUrl.username,
-  password: dbUrl.password,
-  database: dbUrl.pathname.replace("/", ""),
-});
+const { PrismaClient } = prismaPkg;
+const adapter = new PrismaMariaDb(process.env.DATABASE_URL);
 
 export const prisma = new PrismaClient({ adapter });
 
@@ -40,4 +13,4 @@ export const connectPrisma = async () => {
 
 export const disconnectPrisma = async () => {
   await prisma.$disconnect();
-};
+};
